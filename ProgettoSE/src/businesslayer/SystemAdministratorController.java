d001c925d34ef23d24fbdf5c6dd5bf4842c2cd1a
mimparato18, MCALABRESE46 <>
2020-11-30 18:06:33 +0100

    Added Site Class and updated SystemAdministratorController with methods to manage sites and maintenance typologies

--- ProgettoSE/src/businesslayer/SystemAdministratorController.java
+++ ProgettoSE/src/businesslayer/SystemAdministratorController.java
@@ -13,15 +13,16 @@ import java.util.ArrayList;
  * @author marku
  */
 public class SystemAdministratorController {
-    private Database db;
+    private SystemAdministratorDataAccess db;
     private SystemAdministrator admin; 
 
-    public SystemAdministratorController(Database db) {
+    public SystemAdministratorController() {
         this.db = db;         
+        db.Connect();
         admin = new SystemAdministrator("admin","admin");
     }
 
-    
+    //User Management
     public boolean addUser(String username, String password, String role){
         
         if(role=="Mantainer")
@@ -49,5 +50,41 @@ public class SystemAdministratorController {
         return db.getAllUsers(); 
         
     }
+    //Site management
+    
+    public boolean addSite(String branchOffice, String department){
+        return db.createSite(new Site(branchOffice,department));
+    }
+    
+    public boolean updateSite(Site oldSite, String department){
+        return db.modifySite(oldSite, department);
+    }
+    
+    public boolean deleteSite(Site site){
+        return db.removeSite(site);
+    }
+    
+    public ArrayList<Site> viewSites(){
+        return db.getAllSites();
+    }
+    
+    //Typology management
+    
+    public boolean addTypology(String typology){
+        return db.createTypology(typology);
+    }
+    
+    public boolean updateTypology(String oldTypology, String newTypology){
+        return db.modifySite(oldTypology, newTypology);
+    }
+    
+    public boolean deleteTypology(String typology){
+        return db.removeSite(typology);
+    }
+    
+    public ArrayList<String> viewTypologies(){
+        return db.getAllTypologies();
+    }
+
     
 }
