221773463563fa9bb5847a302a21b1d674bec8ce
mimparato18 <m.imparato18@studenti.unisa.it>
2020-12-05 17:54:42 +0100

    Added LoginServiceClass

---
+++ ProgettoSE/src/businesslayer/LoginService.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package businesslayer;
+
+import dataaccesslayer.Login;
+
+/**
+ *
+ * @author marku
+ */
+public class LoginService {
+
+    private Login db;
+
+    public LoginService() {
+        db = new Login();
+        db.connect();
+    }
+    
+    public String checkCredentials(String username, String password){
+        if (db.isUser(username))
+                if (password == db.getPassword(username))
+                        return db.getRole();
+        return null;
+    }
+
+
+}
