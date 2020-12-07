1a77e6beaf6ec2828c11fca0083b17ea75ad59fb
Simone Autorino <s.autorino1@studenti.unisa.it>
2020-12-07 17:49:12 +0100

    Fixed errors in PlannerDataAccess

--- ProgettoSE/src/dataaccesslayer/PlannerDataAccess.java
+++ ProgettoSE/src/dataaccesslayer/PlannerDataAccess.java
@@ -27,7 +27,7 @@ public class PlannerDataAccess extends Database {
         String insertQuery = String.format("INSERT INTO "
                 + "maintenanceactivity(materials, typology, estimatedtime, activitydescription, branchoffice, department, week, interruptable, workspacenotes) "
                 + "values ('%s', '%s', '%d', '%s', '%s', '%s', '%d', '%B', '%s')", activity.getMaterials(), activity.getTypology(), activity.getInterventionTime(), activity.getDescription(),
-                activity.getSite().getBranchOffice(), activity.getSite().getDepartment(), activity.getWeek(), activity.getInterruptable(), activity.getWorkspaceNotes());
+                activity.getSite().getBranchOffice(), activity.getSite().getDepartment(), activity.getWeek(), activity.isInterruptible(), activity.getWorkspaceNotes());
         try {
             statement.executeUpdate(insertQuery);
             return true;
@@ -43,7 +43,7 @@ public class PlannerDataAccess extends Database {
         }
         String updateQuery = String.format("UPDATE maintenanceactivity SET materials = '%s', typology = '%s', estimatedtime = '%d', activitydescription = '%s', branchoffice = '%s', department = '%s', "
                 + "week = '%d', interruptable = '%B', workspacenotes = '%s' WHERE id = '%d'", activity.getMaterials(), activity.getTypology(), activity.getInterventionTime(), activity.getDescription(),
-                activity.getSite().getBranchOffice(), activity.getSite().getDepartment(), activity.getWeek(), activity.getInterruptable(), activity.getWorkspaceNotes(), id);
+                activity.getSite().getBranchOffice(), activity.getSite().getDepartment(), activity.getWeek(), activity.isInterruptible(), activity.getWorkspaceNotes(), id);
         try {
             statement.executeUpdate(updateQuery);
             return true;
@@ -70,7 +70,7 @@ public class PlannerDataAccess extends Database {
     public ArrayList<MaintenanceActivity> getAllActivities() {
         String materials, typology, activityDescription, branchOffice, department, workspaceNotes;
         int id, interventionTime, week;
-        boolean interruptable;
+        boolean interruptible;
 
         ArrayList<MaintenanceActivity> activitiesList = new ArrayList<>();
         String selectQuery = String.format("SELECT * FROM maintenanceactivity");
@@ -85,12 +85,12 @@ public class PlannerDataAccess extends Database {
                 branchOffice = resultSet.getString(6);
                 department = resultSet.getString(7);
                 week = resultSet.getInt(8);
-                interruptable = resultSet.getBoolean(9);
+                interruptible = resultSet.getBoolean(9);
                 workspaceNotes = resultSet.getString(10);
 
                 Site site = new Site(branchOffice, department);
 
-                activitiesList.add(new MaintenanceActivity(id, materials, typology, interventionTime, activityDescription, site, week, interruptable, workspaceNotes));
+                activitiesList.add(new MaintenanceActivity(id, site, typology, activityDescription, interventionTime, interruptible, materials, week, workspaceNotes));
             }
             return activitiesList;
         } catch (SQLException e) {
